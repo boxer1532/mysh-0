c4e75b8cc2ba5eee1c4f35c590a5d9f24e12005e
boxer1532 <boxer1532@naver.com>
2017-10-07 14:56:51 +0900

    OS assignment-0

--- src/commands.c
+++ src/commands.c
@@ -1,12 +1,21 @@
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
+ 
+  int res;
+  char buf[9000];
 
-  // TODO: Fill it!
+  res = chdir(argv[1]);
+
+  if(res == -1)
+    return -1;
+  
   return 0;
 }
 
@@ -14,17 +23,33 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+
+  char buf[9000];
+
+  if(getcwd(buf, 9000) == NULL)
+     return -1;
+  else
+     printf("%s",buf);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+
+  if(argc != 2)
+   return 0;
+  else if(strcmp(argv[0], "cd"))
+   return 0;
+  else
+   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  
+ if(argc != 1 || (argc == 1 && strcmp(argv[0],"pwd")))
+  return 0;
+ else if(argc == 1 && !strcmp(argv[0],"pwd"))
   return 1;
+ else
+  return 0;
 }
